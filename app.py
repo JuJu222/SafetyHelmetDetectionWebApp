10c09ee196fe2dcd5a6611e3a25f328e10d1018d
JuJu222 <64346619+JuJu222@users.noreply.github.com>
2024-03-26 14:51:54 +0700

    Added base views

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_mysqldb import MySQL
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'flask'
+ 
+mysql = MySQL(app)
 
 @app.route('/')
 def hello():
-    return '<h1>Hello, World!</h1>'
+    return render_template('login.html')
