711cf769adf83f0f156c1666ea36edd6674d4572
JuJu222 <64346619+JuJu222@users.noreply.github.com>
2024-04-10 20:40:53 +0700

    Implemented layout

--- app.py
+++ app.py
@@ -2,17 +2,20 @@ from flask import Flask, render_template
 from flask_mysqldb import MySQL
 
 app = Flask(__name__, template_folder='templates')
+app.static_folder = 'static'
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'flask'
- 
+
 mysql = MySQL(app)
+
 
 @app.route('/login')
 def login():
     return render_template('login.html')
+
 
 @app.route('/')
 def index():
