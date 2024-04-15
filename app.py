a9cc4d296cf23c714a061d7360aec713a498489b
JuJu222 <64346619+JuJu222@users.noreply.github.com>
2024-04-15 18:44:44 +0700

    Implemented model detection

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
+from subprocess import Popen
 
 from video import extractImages
 
 app = Flask(__name__, template_folder='templates')
 app.static_folder = 'static'
 
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'safety_helmet_app'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
@@ -32,11 +33,17 @@ def index():
 @app.route('/upload', methods = ['POST'])
 def upload():
     if request.method == 'POST':   
-        print(request)
-        f = request.files['file'] 
-        f.save(f.filename)  
+        # print(request)
+        # f = request.files['file'] 
+        # f.save(f.filename)  
 
-        extractImages(f.filename, "frames")
+        # extractImages(f.filename, "frames")
+
+        process = Popen(["python", "detect.py", '--source', "frame0.jpg", "--weights","last.pt"], shell=False)
+        process.communicate()
+        process.wait()
+
+        print("adadada")
 
         return "aaa"   
 
